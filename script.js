7ce07fb3e3fb17298e84c2162d2fc5a38397ff45
Kauan Santos <kauansantos83@gmail.com>
2021-07-05 15:56:23 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+var div = document.getElementById('log');
+var textos = [ 'Estudante','Web Developer'];
+
+function escrever(str, done) {
+    var char = str.split('').reverse();
+    var typer = setInterval(function() {
+        if (!char.length) {
+            clearInterval(typer);
+            return setTimeout(done, 500); // só para esperar um bocadinho
+        }
+        var next = char.pop();
+        div.innerHTML += next;
+    }, 100);
+}
+
+function limpar(done) {
+    var char = div.innerHTML;
+    var nr = char.length;
+    var typer = setInterval(function() {
+        if (nr-- == 0) {
+            clearInterval(typer);
+            return done();
+        }
+        div.innerHTML = char.slice(0, nr);
+    }, 100);
+}
+
+
+function rodape(conteudos, el) {
+    var atual = -1;
+	function prox(cb){
+		if (atual < conteudos.length - 1) atual++;
+		else atual = 0;
+		var str = conteudos[atual];
+		escrever(str, function(){
+			limpar(prox);
+		});
+	}
+	prox(prox);
+}
+rodape(textos);
+
+
+
+
